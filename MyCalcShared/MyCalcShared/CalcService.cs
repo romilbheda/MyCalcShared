df41a480b37bc03f2d98c4aceeea0766fb8a5f86
romilbheda <romilbheda@hotmail.com>
2020-12-23 20:26:26 +0530

    CalcShared Project Added

---
+++ MyCalcShared/MyCalcShared/CalcService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyCalcShared
+{
+    public class CalcService
+    {
+        static public int AddValue(int prodA, int prodB)
+        {
+            int total1, total2, finaltotal;
+
+            total1 = prodA + prodB;
+            total2 = (total1 * 5) / 100;
+            finaltotal = total1 - total2;
+            return finaltotal;
+        }
+    }
+}
